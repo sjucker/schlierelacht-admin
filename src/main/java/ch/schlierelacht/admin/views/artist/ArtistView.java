3e62c5eae54f316957c7a3a0dec338ac935ed02c
Stefan Jucker <stefan.jucker@gmail.com>
2026-06-29 17:58:18 +0200

    Add missing ArtistView

--- src/main/java/ch/schlierelacht/admin/views/artist/ArtistView.java
+++ src/main/java/ch/schlierelacht/admin/views/artist/ArtistView.java
@@ -3,6 +3,7 @@ package ch.schlierelacht.admin.views.artist;
 import ch.schlierelacht.admin.dto.AttractionType;
 import ch.schlierelacht.admin.jooq.tables.daos.AttractionDao;
 import ch.schlierelacht.admin.jooq.tables.daos.TagDao;
+import ch.schlierelacht.admin.service.AttractionFileService;
 import ch.schlierelacht.admin.service.CloudflareService;
 import ch.schlierelacht.admin.views.AbstractAttractionView;
 import ch.schlierelacht.admin.views.MainLayout;
@@ -19,8 +20,9 @@ import org.jspecify.annotations.NonNull;
 @PermitAll
 public class ArtistView extends AbstractAttractionView {
 
-    public ArtistView(AttractionDao attractionDao, TagDao tagDao, CloudflareService cloudflareService, DSLContext dslContext) {
-        super(attractionDao, tagDao, cloudflareService, dslContext);
+    public ArtistView(AttractionDao attractionDao, TagDao tagDao, CloudflareService cloudflareService,
+                      DSLContext dslContext, AttractionFileService attractionFileService) {
+        super(attractionDao, tagDao, cloudflareService, dslContext, attractionFileService);
     }
 
     @Override
